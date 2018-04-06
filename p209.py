723cd1572358b9cf77d9f12504620093a541d14f
mc.meng <menghaocheng@qq.com>
2018-04-06 14:34:03 +0800

    conv2d

---
+++ p209.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+
+img = cv2.imread("data/lena.jpg")
+img = np.array(img, dtype=np.float32)
+x_image=tf.reshape(img, [1, 512, 512, 3])
+
+filter = tf.Variable(tf.ones([9, 9, 3, 1]))
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    res = tf.nn.conv2d(x_image, filter, strides=[1, 2, 2, 1], padding="SAME")
+    res_image = sess.run(tf.reshape(res, [256, 256]))/128 + 1
+
+cv2.imshow("data/lena", res_image.astype("uint8"))
+cv2.waitKey()
+cv2.destroyAllWindows()
